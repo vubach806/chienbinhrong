95eebc2de07870a21f2b916c5e73659fed6dbd2f
bachkane79 <bachkane79@gmail.com>
2025-02-15 00:36:40 +0700

    Ịt ẹ anh yêu em GH valentin 2025

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+onload = () =>{
+    document.body.classList.remove("container");
+};
